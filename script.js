2b517f9ac8110878942e493d5acfae27cc2bbc60
NICKPOLUKITAS <162640270+NICKPOLUKITAS@users.noreply.github.com>
2025-05-03 22:13:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função que mostra mais informações quando o botão é clicado
+function showMoreInfo() {
+    alert("O Palmeiras é um dos maiores times de futebol do Brasil e tem uma grande história, incluindo múltiplos títulos nacionais e internacionais.");
+}
